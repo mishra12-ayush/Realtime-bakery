f7474fd59e5fcce0c649ba7e2e38073ce858d7ca
Ayush mishra <mishraayush91.2@gmail.com>
2020-09-03 02:20:38 +0530

    menu from server

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,7 +3,7 @@ const Burger = require('../../models/burger');
 function homeController() {
     return {
         index(req, res){
-            Burger.find({}).then(function(burgers) {
+            Burger.find({}).then(function( burgers) {
                 console.log(burgers);
                 return res.render('home', { burgers: burgers });
              });
